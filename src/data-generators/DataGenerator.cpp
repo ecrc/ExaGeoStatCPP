f8b4a3147299d16c6e1389096ef70c85425ec2d9
Mahmoud Elkarargy <mahmoud.elkarargy@brightskiesinc.com>
2023-02-23 09:56:52 +0200

    Milestone 2

---
+++ src/data-generators/DataGenerator.cpp
@@ -0,0 +1,13 @@
+
+/**
+ * @file DataGenerator.cpp
+ *
+ * @version 1.0.0
+ * @author Sameh Abdulah
+ * @date 2023-02-14
+**/
+
+#include <data-generators/DataGenerator.hpp>
+
+using namespace exageostat::generators;
+
