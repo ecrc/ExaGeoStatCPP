b1f0fbf9cf6d9474cba94223cb5141d38c219499
Sameh Abdulah <abdullsm@kw60194.kaust.edu.sa>
2024-01-16 15:44:34 +0300

    add test file for pow-exp kernel

---
+++ tests/cpp-tests/kernels/concrete/TestUnivariatePowExpStationary.cpp
@@ -0,0 +1,73 @@
+
+// Copyright (c) 2017-2023 King Abdullah University of Science and Technology,
+// All rights reserved.
+// ExaGeoStat is a software package, provided by King Abdullah University of Science and Technology (KAUST).
+
+/**
+ * @file TestUnivariatePowExpStationary.cpp
+ * @brief Unit tests for the TestUnivariatePowExpStationary kernel in the ExaGeoStat software package.
+ * @details This file contains Catch2 unit tests that validate the functionality of the TestUnivariatePowExpStationary kernel
+ * in the ExaGeoStat software package. The tests cover the generation of data using this kernel with various configurations.
+ * @version 1.0.0
+ * @author Mahmoud ElKarargy
+ * @author Sameh Abdulah
+ * @date 2024-01-16
+**/
+
+#include <catch2/catch_all.hpp>
+#include <api/ExaGeoStat.hpp>
+#include <hardware/ExaGeoStatHardware.hpp>
+
+using namespace std;
+
+using namespace exageostat::configurations;
+using namespace exageostat::api;
+using namespace exageostat::common;
+using namespace exageostat::hardware;
+
+void TEST_KERNEL_GENERATION_UnivariatePowExpStationary() {
+
+    SECTION("UnivariatePowExpStationary")
+    {
+
+        // Create a new synthetic_data_configurations object with the provided command line arguments
+        Configurations synthetic_data_configurations;
+
+        int N = 9;
+        synthetic_data_configurations.SetProblemSize(N);
+        synthetic_data_configurations.SetKernelName("UnivariatePowExpStationary");
+        synthetic_data_configurations.SetDimension(Dimension2D);
+
+        vector<double> initial_theta{1, 0.1, 0.5};
+        synthetic_data_configurations.SetInitialTheta(initial_theta);
+
+        int dts = 5;
+        synthetic_data_configurations.SetDenseTileSize(dts);
+        synthetic_data_configurations.SetComputation(EXACT_DENSE);
+        // initialize ExaGeoStat Hardware.
+        auto hardware = ExaGeoStatHardware(EXACT_DENSE, 3, 0);
+
+        int seed = 0;
+        srand(seed);
+        exageostat::dataunits::ExaGeoStatData<double> data;
+        exageostat::api::ExaGeoStat<double>::ExaGeoStatLoadData(hardware, synthetic_data_configurations,
+                                                                data);
+
+        auto *CHAM_descriptorZ = data.GetDescriptorData()->GetDescriptor(exageostat::common::CHAMELEON_DESCRIPTOR,
+                                                                         exageostat::common::DESCRIPTOR_Z).chameleon_desc;
+        auto *A = (double *) CHAM_descriptorZ->mat;
+        // Define the expected output
+        double expected_output_data[] = { -1.272336, -2.362813, 0.616384, -0.072468, 0.401498, -1.559690, 0.211848, 
+                                          0.776627, -1.524810};
+
+        for (size_t i = 0; i < N; i++) {
+            double diff = A[i] - expected_output_data[i];
+            REQUIRE(diff == Catch::Approx(0.0).margin(1e-6));
+        }
+    }
+}
+
+TEST_CASE("Univariate Pow-Exp Stationary kernel test") {
+    TEST_KERNEL_GENERATION_UnivariatePowExpStationary();
+
+}
